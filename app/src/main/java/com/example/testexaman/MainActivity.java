c78bfea9684ebc16d2f6bb95ca72e43fc007ab07
MONTASSER AL AMSAKI <alamsakimontasser@gmail.com>
2022-05-29 13:55:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/testexaman/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.testexaman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+     EditText t1,t2,t3;
+
+     Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t1=findViewById(R.id.t1);
+        t2=findViewById(R.id.t2);
+        t3=findViewById(R.id.t3);
+        b1=findViewById(R.id.b1);
+    }
+
+    public void click(View view) {
+        t3.setText((t2.getText().toString()+t1.getText().toString()));
+    }
+}
