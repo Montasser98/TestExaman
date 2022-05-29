7c38a7278e3ca6a34475af6d0ae6822e2396cefd
MONTASSER AL AMSAKI <alamsakimontasser@gmail.com>
2022-05-29 18:50:43 +0100

    Les Calcules

--- app/src/main/java/com/example/testexaman/MainActivity.java
+++ app/src/main/java/com/example/testexaman/MainActivity.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
      EditText t1,t2,t3;
-
+     RadioGroup rg;
      Button b1;
+     RadioButton r1,r2,r3,r4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +28,46 @@ public class MainActivity extends AppCompatActivity {
         t2=findViewById(R.id.t2);
         t3=findViewById(R.id.t3);
         b1=findViewById(R.id.b1);
+        rg=findViewById(R.id.rg);
+        r1=findViewById(R.id.r1);
+        r2=findViewById(R.id.r2);
+        r3=findViewById(R.id.r3);
+        r4=findViewById(R.id.r4);
+
+
     }
 
     public void click(View view) {
         float a=Float.valueOf(t2.getText().toString());
         float b=Float.valueOf(t1.getText().toString());
-        int aa= Integer.valueOf(t2.getText().toString());
-        int bb=Integer.valueOf(t1.getText().toString());
+        float c=0;
 
-        t3.setText(aa+bb+"");
+        if(r1.isChecked()) {
+            c=a+b;
+        }
+        if(r2.isChecked()){
+            c=a-b;
+        }
+        if(r3.isChecked()){
+            c=a*b;
+        }
+        if(r4.isChecked()){
+            if(a<b){
+            c=a/b;
+            }
+            else {
+                Toast.makeText(MainActivity.this,"C'ant meke this calcule",Toast.LENGTH_LONG).show();
+
+            }
+            }
+        t3.setText(c+"");
+        }
+
+    public void vide(View view) {
+        r1.setChecked(true);
+        t1.getText().clear();
+        t2.getText().clear();
+        t3.getText().clear();
+        t1.requestFocus();
     }
 }
